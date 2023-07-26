be17ed15b3efab964ae1358ccd4d5cc046fd7029
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-26 19:14:41 +0400

    0

---
+++ .nuxt/dist/server/styles.mjs
@@ -0,0 +1,8 @@
+const interopDefault = r => r.default || r || []
+export default {
+  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.063c41a8.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.cf00f4cc.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a001b7ed.mjs').then(interopDefault),
+  "pages/index.vue": () => import('./_nuxt/index-styles.36cd4ece.mjs').then(interopDefault),
+  "layouts/default.vue": () => import('./_nuxt/default-styles.e21f95e0.mjs').then(interopDefault)
+}
