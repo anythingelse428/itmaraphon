400320e97544a2b0cbb806f2ce1713599c905ef0
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 00:07:25 +0400

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  mode: 'ssr',
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    },
+    baseURL: '/itmaraphon/'
+  },
+  app: {
+    baseURL: '/itmaraphon/' // baseURL: '/<repository>/'
+  },
   devtools: { enabled: false },
   vite: {
     css: {
