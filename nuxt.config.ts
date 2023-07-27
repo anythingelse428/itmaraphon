198549143cd7cce1c8b0bd9d15b26cb437c8102d
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 00:17:23 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    prerender: {
-      crawlLinks: true
-    },
-    baseURL: '/itmaraphon/'
-  },
   app: {
     baseURL: '/itmaraphon/' // baseURL: '/<repository>/'
   },
