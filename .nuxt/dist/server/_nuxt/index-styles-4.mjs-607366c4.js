6a3da8f75c918c2c064c009d653f947791e6c69b
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 02:54:17 +0400

    1

---
+++ .nuxt/dist/server/_nuxt/index-styles-4.mjs-607366c4.js
@@ -0,0 +1,5 @@
+const HistoryLine_vue_vue_type_style_index_0_scoped_d859583e_lang = ".history[data-v-d859583e]{display:flex;flex-wrap:nowrap;padding-bottom:30vh;position:relative;z-index:5}.history__row[data-v-d859583e]{position:relative;width:100%}.history__pagination[data-v-d859583e]{align-items:center;bottom:0;display:flex;height:100vh;left:60px;position:fixed;top:0;z-index:100}@media screen and (max-width:1350px){.history__pagination[data-v-d859583e]{display:none}}";
+export {
+  HistoryLine_vue_vue_type_style_index_0_scoped_d859583e_lang as default
+};
+//# sourceMappingURL=index-styles-4.mjs-607366c4.js.map
