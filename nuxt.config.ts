b7f5cf39b943ca570f70965dc2d2070fca0d4172
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-27 23:37:15 +0400

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   mode: 'ssr',
-   router: {
-      base: '/anythingelse428.github.io/'
-   },
+  router: {
+    base: '/anythingelse428.github.io/'
+  },
+  app: {
+    baseURL: '/anythingelse428.github.io/'
+  },
   devtools: { enabled: false },
   vite: {
     css: {
