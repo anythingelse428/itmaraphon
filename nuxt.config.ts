0761a2a3f1ed168e30117bc65657caf45187c850
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-27 23:33:17 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  mode: 'static',
+  mode: 'ssr',
    router: {
       base: '/anythingelse428.github.io/'
    },
