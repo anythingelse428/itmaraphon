fa940da0d4bcfb8986a8ceb3616416beee5ecfc1
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 00:57:22 +0400

    0

---
+++ .nuxt/dist/server/_nuxt/index-styles-2.mjs-0e2014cd.js
@@ -0,0 +1,5 @@
+const PaginationNav_vue_vue_type_style_index_0_scoped_e5f8574e_lang = ".pagination[data-v-e5f8574e]{display:flex;flex-direction:column;gap:24px}";
+export {
+  PaginationNav_vue_vue_type_style_index_0_scoped_e5f8574e_lang as default
+};
+//# sourceMappingURL=index-styles-2.mjs-0e2014cd.js.map
