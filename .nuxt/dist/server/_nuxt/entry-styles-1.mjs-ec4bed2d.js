cd577dd4677407bff4b9cef274197ef53e2df470
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 00:40:16 +0400

    Updates

--- .nuxt/dist/server/_nuxt/entry-styles-1.mjs-ec4bed2d.js
+++
@@ -1,5 +0,0 @@
-const app_vue_vue_type_style_index_0_lang = '@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@400;600&display=swap");*{box-sizing:border-box;color:#f2f5ff;font-family:Manrope,sans-serif;margin:0;padding:0;scroll-behavior:smooth}* a{text-decoration:none}';
-export {
-  app_vue_vue_type_style_index_0_lang as default
-};
-//# sourceMappingURL=entry-styles-1.mjs-ec4bed2d.js.map
