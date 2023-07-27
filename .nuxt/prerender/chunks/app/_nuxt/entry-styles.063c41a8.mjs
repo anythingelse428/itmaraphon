b22ce458a653b6311fba0d2783659413881fa83a
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-27 23:57:38 +0400

    1

---
+++ .nuxt/prerender/chunks/app/_nuxt/entry-styles.063c41a8.mjs
@@ -0,0 +1,6 @@
+const app_vue_vue_type_style_index_0_lang = '@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@400;600&display=swap");*{box-sizing:border-box;color:#f2f5ff;font-family:Manrope,sans-serif;margin:0;padding:0;scroll-behavior:smooth}* a{text-decoration:none}';
+
+const entryStyles_063c41a8 = [app_vue_vue_type_style_index_0_lang];
+
+export { entryStyles_063c41a8 as default };
+//# sourceMappingURL=entry-styles.063c41a8.mjs.map
