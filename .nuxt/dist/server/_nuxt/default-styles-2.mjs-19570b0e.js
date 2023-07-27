a05265517e97cdd9e480915d6e740c628d2c42e9
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 03:04:13 +0400

    0

--- .nuxt/dist/server/_nuxt/default-styles-2.mjs-19570b0e.js
+++
@@ -1,5 +0,0 @@
-const default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang = ".layout[data-v-2a5b62d5]{margin:0;padding:0}.layout.sticky[data-v-2a5b62d5]{padding-top:60px}.layout.--default[data-v-2a5b62d5]{background:radial-gradient(50% 50% at 50% 50%,rgba(18,27,62,0) 0,rgba(8,12,21,.5) 100%),linear-gradient(0deg,#121b3e,#121b3e),#fff;min-height:100vh}.layout.--default .container[data-v-2a5b62d5]{margin:0 auto;max-width:100%;width:min(1500px,100%)}@media screen and (max-width:1550px){.layout.--default .container[data-v-2a5b62d5]{width:min(1200px,100%)}}@media screen and (max-width:1250px){.layout.--default .container[data-v-2a5b62d5]{width:min(992px,100%)}}@media screen and (max-width:1000px){.layout.--default .container[data-v-2a5b62d5]{width:min(780px,100%)}}@media screen and (max-width:800px){.layout.--default .container[data-v-2a5b62d5]{width:95%}}";
-export {
-  default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang as default
-};
-//# sourceMappingURL=default-styles-2.mjs-19570b0e.js.map
