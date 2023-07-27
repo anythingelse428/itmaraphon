3e4b95551d12d21054388d75da9809b50b55299a
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 02:47:58 +0400

    1

---
+++ .nuxt/prerender/chunks/app/_nuxt/default-styles.6a8f1401.mjs
@@ -0,0 +1,31 @@
+import { p as publicAssetsURL } from '../../renderer.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/vue-bundle-renderer/dist/runtime.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/h3/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/devalue/index.js';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/ufo/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/vue/server-renderer/index.mjs';
+import '../../nitro/nitro-prerenderer.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/node-fetch-native/dist/polyfill.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/ofetch/dist/node.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/destr/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/unenv/runtime/fetch/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/hookable/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/scule/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/klona/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/defu/dist/defu.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/ohash/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/unstorage/dist/index.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/unstorage/drivers/fs.mjs';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/radix3/dist/index.mjs';
+import 'node:fs';
+import 'node:url';
+import 'file://C:/Users/anythingelse428/Documents/GitHub/itmaraphon/node_modules/pathe/dist/index.mjs';
+
+const NavigationMenu_vue_vue_type_style_index_0_scoped_08d6b971_lang = ".nav[data-v-08d6b971]{align-items:center;-webkit-backdrop-filter:blur(3.5px);backdrop-filter:blur(3.5px);background:rgba(17,25,56,.01);display:flex;flex-direction:row;flex-wrap:nowrap;padding:15px 65px}.nav *[data-v-08d6b971]{position:relative;transition:all .5s cubic-bezier(.445,.05,.55,.95);z-index:4}.nav-toggler[data-v-08d6b971]{cursor:pointer;display:none}@media screen and (max-width:1000px){.nav[data-v-08d6b971]{justify-content:space-between;padding:15px 90px}.nav-toggler[data-v-08d6b971]{display:block;height:54px;width:54px}.nav-toggler[data-v-08d6b971]:before{content:url(" + publicAssetsURL("assets/icons/burger.svg") + ");display:block;inset:0;position:absolute}.nav-toggler.active[data-v-08d6b971]:before{content:url(" + publicAssetsURL("assets/icons/x.svg") + ")}.nav-tool[data-v-08d6b971]{display:none}.nav-collapse[data-v-08d6b971]{background:#121b3e;display:block;height:111vh;inset:0;padding:165px 99px 0;position:absolute}.nav-collapse[data-v-08d6b971]:not(.active){margin-top:-150vh}.nav-collapse.active>.nav-links[data-v-08d6b971]{align-content:flex-start;align-items:flex-start;display:flex;flex-direction:column;flex-wrap:wrap;gap:42px 25%;height:14em;inset:0;justify-content:flex-start;position:relative}.nav-collapse.active>.nav-links .nav-link[data-v-08d6b971]{color:#ecf1ff;font-size:16px;font-weight:400;line-height:24px}}@media screen and (max-width:800px){.nav[data-v-08d6b971]{padding:11px 10px}.nav-collapse[data-v-08d6b971]{padding:110px 12px}}@media screen and (max-width:550px){.nav-collapse.active>.nav-links[data-v-08d6b971]{gap:24px 5%}}.nav.sticky[data-v-08d6b971]{position:fixed;top:0;width:100%;z-index:500}.nav-brand[data-v-08d6b971]{margin-right:87px;position:relative;z-index:7}@media screen and (max-width:1550px){.nav-brand[data-v-08d6b971]{margin-right:25px}}.nav-links[data-v-08d6b971]{align-items:center;display:flex;flex-direction:row;gap:64px}@media screen and (max-width:1550px){.nav-links[data-v-08d6b971]{gap:32px}}@media screen and (max-width:1350px){.nav-links[data-v-08d6b971]{gap:18px}}.nav-link[data-v-08d6b971]{color:#fafbff;cursor:pointer;font-size:18px;font-style:normal;font-weight:600;line-height:130%;transition:all .1s}.nav-link[data-v-08d6b971]:hover{color:#b7bbc4}@media screen and (max-width:1200px){.nav-link[data-v-08d6b971]{color:#ecf1ff;font-size:16px;line-height:24px}}.nav-tools[data-v-08d6b971]{align-items:center;display:flex;flex-direction:row;gap:35px;margin-left:auto;position:relative;z-index:7}@media screen and (max-width:1550px){.nav-tools[data-v-08d6b971]{gap:32px}}@media screen and (max-width:1350px){.nav-tools[data-v-08d6b971]{gap:20px}}";
+
+const default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang = ".layout[data-v-2a5b62d5]{margin:0;padding:0}.layout.sticky[data-v-2a5b62d5]{padding-top:60px}.layout.--default[data-v-2a5b62d5]{background:radial-gradient(50% 50% at 50% 50%,rgba(18,27,62,0) 0,rgba(8,12,21,.5) 100%),linear-gradient(0deg,#121b3e,#121b3e),#fff;min-height:100vh}.layout.--default .container[data-v-2a5b62d5]{margin:0 auto;max-width:100%;width:min(1500px,100%)}@media screen and (max-width:1550px){.layout.--default .container[data-v-2a5b62d5]{width:min(1200px,100%)}}@media screen and (max-width:1250px){.layout.--default .container[data-v-2a5b62d5]{width:min(992px,100%)}}@media screen and (max-width:1000px){.layout.--default .container[data-v-2a5b62d5]{width:min(780px,100%)}}@media screen and (max-width:800px){.layout.--default .container[data-v-2a5b62d5]{width:95%}}";
+
+const defaultStyles_6a8f1401 = [NavigationMenu_vue_vue_type_style_index_0_scoped_08d6b971_lang, default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang, default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang];
+
+export { defaultStyles_6a8f1401 as default };
+//# sourceMappingURL=default-styles.6a8f1401.mjs.map
