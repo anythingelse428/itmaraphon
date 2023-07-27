7661da4cd945187b5156f6fba2ec8739f002a58d
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 00:30:37 +0400

    0

---
+++ .nuxt/dist/server/_nuxt/default-f60a1ac7.js
@@ -0,0 +1,82 @@
+import { mergeProps, useSSRContext } from "vue";
+import { ssrRenderAttrs, ssrRenderAttr, ssrRenderClass, ssrRenderComponent, ssrRenderSlot } from "vue/server-renderer";
+import { _ as _export_sfc } from "../server.mjs";
+import "ofetch";
+import "#internal/nitro";
+import "hookable";
+import "unctx";
+import "vue-router";
+import "h3";
+import "ufo";
+import "devalue";
+import "@unhead/ssr";
+import "unhead";
+import "@unhead/shared";
+import "destr";
+import "klona";
+import "defu";
+const _sfc_main$1 = {
+  name: "NavigationMenu",
+  data() {
+    return {
+      isOpened: false
+    };
+  },
+  methods: {
+    doSticky() {
+      var _a;
+      const $nav = this.$refs.nav;
+      if (window.scrollY >= $nav.offsetTop) {
+        $nav.classList.add("sticky");
+        (_a = $nav.parentElement) == null ? void 0 : _a.classList.add("sticky");
+      } else {
+        $nav.classList.remove("sticky");
+      }
+    }
+  },
+  mounted() {
+    this.doSticky();
+  }
+};
+const _imports_0 = "" + __publicAssetsURL("assets/logo.svg");
+const _imports_1 = "" + __publicAssetsURL("assets/icons/search.svg");
+const _imports_2 = "" + __publicAssetsURL("assets/icons/blind.svg");
+const NavigationMenu_vue_vue_type_style_index_0_scoped_6c009198_lang = "";
+function _sfc_ssrRender$1(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  _push(`<nav${ssrRenderAttrs(mergeProps({
+    class: "nav",
+    ref: "nav"
+  }, _attrs))} data-v-6c009198><div class="nav-brand" data-v-6c009198><img${ssrRenderAttr("src", _imports_0)} width="103" height="60" alt="УавиаК-МЦК" srcset="" data-v-6c009198></div><div class="${ssrRenderClass([$data.isOpened && "active", "nav-collapse"])}" data-v-6c009198><div class="nav-links" data-v-6c009198><div class="nav-link" data-v-6c009198>О колледже</div><div class="nav-link" data-v-6c009198>Абитуриентам</div><div class="nav-link" data-v-6c009198>Студентам</div><div class="nav-link" data-v-6c009198>Преподавателям</div><div class="nav-link" data-v-6c009198>Трудоустройство</div><div class="nav-link" data-v-6c009198>Автошкола</div></div></div><div class="nav-tools" data-v-6c009198><div class="nav-tool" data-v-6c009198><img${ssrRenderAttr("src", _imports_1)} alt="Поиск" width="18" height="18" data-v-6c009198></div><div class="nav-tool" data-v-6c009198><img${ssrRenderAttr("src", _imports_2)} alt="Версия для слабовидящих" width="26" height="18" data-v-6c009198></div><div class="${ssrRenderClass([$data.isOpened && "active", "nav-toggler"])}" data-v-6c009198></div></div></nav>`);
+}
+const _sfc_setup$1 = _sfc_main$1.setup;
+_sfc_main$1.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("components/NavigationMenu.vue");
+  return _sfc_setup$1 ? _sfc_setup$1(props, ctx) : void 0;
+};
+const __nuxt_component_0 = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["ssrRender", _sfc_ssrRender$1], ["__scopeId", "data-v-6c009198"]]);
+const _sfc_main = {
+  components: {
+    NavigationMenu: __nuxt_component_0
+  }
+};
+const default_vue_vue_type_style_index_0_scoped_2a5b62d5_lang = "";
+function _sfc_ssrRender(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  const _component_NavigationMenu = __nuxt_component_0;
+  _push(`<div${ssrRenderAttrs(mergeProps({ class: "layout --default" }, _attrs))} data-v-2a5b62d5>`);
+  _push(ssrRenderComponent(_component_NavigationMenu, null, null, _parent));
+  _push(`<main class="container" data-v-2a5b62d5>`);
+  ssrRenderSlot(_ctx.$slots, "default", {}, null, _push, _parent);
+  _push(`</main><footer data-v-2a5b62d5></footer></div>`);
+}
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("layouts/default.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+const _default = /* @__PURE__ */ _export_sfc(_sfc_main, [["ssrRender", _sfc_ssrRender], ["__scopeId", "data-v-2a5b62d5"]]);
+export {
+  _default as default
+};
+//# sourceMappingURL=default-f60a1ac7.js.map
---
+++ .nuxt/dist/server/_nuxt/default-styles.3b076d38.mjs
@@ -0,0 +1,4 @@
+import style_0 from './default-styles-1.mjs-74edd5a2.js';
+import style_1 from './default-styles-2.mjs-19570b0e.js';
+import style_2 from './default-styles-2.mjs-19570b0e.js';
+export default [style_0, style_1, style_2]
