d6f2579732088d99bf9c7286610cb2b6e5348e21
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-29 03:24:50 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,11 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       title:'УавиаК-МЦК',
       link:[
-        {rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png'},
-        {rel: 'icon', type:"image/png", sizes: "32x32", href: '/favicon-32x32.png'},
-        {rel: 'icon', type:"image/png", sizes: "16x16", href: '/favicon-16x16.png'},
-        {rel: 'manifest', href: '/site.webmanifest'},
-        {rel: 'mask-icon', color: "#5bbad5", href: '/safari-pinned-tab.svg'},
+        {rel: 'apple-touch-icon', sizes: "180x180", href: 'apple-touch-icon.png'},
+        {rel: 'icon', type:"image/png", sizes: "32x32", href: 'favicon-32x32.png'},
+        {rel: 'icon', type:"image/png", sizes: "16x16", href: 'favicon-16x16.png'},
+        {rel: 'manifest', href: 'site.webmanifest'},
+        {rel: 'mask-icon', color: "#5bbad5", href: 'safari-pinned-tab.svg'},
       ],
       noscript: [
         { children: 'JavaScript в вашем браузере отключен, пожалуйста, включите JavaScript или зайдите через другой браузер' }
