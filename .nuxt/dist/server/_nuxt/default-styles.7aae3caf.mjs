51e8cf1bf9a731a1490979ed966b5a4ab77d3b22
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 19:14:40 +0400

    1

--- .nuxt/dist/server/_nuxt/default-styles.7aae3caf.mjs
+++
@@ -1,5 +0,0 @@
-import style_0 from './default-styles-1.mjs-42a3eea3.js';
-import style_1 from './default-styles-2.mjs-073c6d4c.js';
-import style_2 from './default-styles-3.mjs-0494fdd3.js';
-import style_3 from './default-styles-3.mjs-0494fdd3.js';
-export default [style_0, style_1, style_2, style_3]
--- .nuxt/dist/server/_nuxt/index-styles-1.mjs-b742b7b8.js
+++
@@ -1,5 +0,0 @@
-const PaginationBullet_vue_vue_type_style_index_0_scoped_982e0dfd_lang = '.pagination-bullet[data-v-982e0dfd]{align-items:center;background:transparent;border-radius:100%;cursor:pointer;display:flex;height:24px;justify-content:center;margin:0 auto;position:relative;width:24px}.pagination-bullet[data-v-982e0dfd]:before{background:#314373;border-radius:100%;content:"";display:block;height:12px;position:relative;width:12px;z-index:5}.pagination-bullet.active[data-v-982e0dfd]{background:#1849a0}.pagination-bullet.active[data-v-982e0dfd]:before{background:#fcfdfd}';
-export {
-  PaginationBullet_vue_vue_type_style_index_0_scoped_982e0dfd_lang as default
-};
-//# sourceMappingURL=index-styles-1.mjs-b742b7b8.js.map
