a0547af35a5cbb13492083141507d7467750c056
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-28 17:28:58 +0400

    del

--- .nuxt/dist/server/_nuxt/index-styles.23277678.mjs
+++
@@ -1,7 +0,0 @@
-import style_0 from './index-styles-1.mjs-9a55e729.js';
-import style_1 from './index-styles-2.mjs-0e2014cd.js';
-import style_2 from './index-styles-3.mjs-ab27914d.js';
-import style_3 from './index-styles-4.mjs-80cd1d76.js';
-import style_4 from './index-styles-5.mjs-d0351e0a.js';
-import style_5 from './index-styles-5.mjs-d0351e0a.js';
-export default [style_0, style_1, style_2, style_3, style_4, style_5]
