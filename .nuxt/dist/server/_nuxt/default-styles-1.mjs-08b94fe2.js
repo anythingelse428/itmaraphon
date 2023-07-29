1da0d017a610652d03cddd644c68d914c78d6096
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-29 04:56:59 +0400

    0

---
+++ .nuxt/dist/server/_nuxt/default-styles-1.mjs-08b94fe2.js
@@ -0,0 +1,5 @@
+const NavigationLink_vue_vue_type_style_index_0_scoped_6fa66696_lang = ".nav-link[data-v-6fa66696]{color:#fafbff;cursor:pointer;font-size:18px;font-style:normal;font-weight:600;line-height:130%;transition:all .1s}.nav-link[data-v-6fa66696]:hover{color:#b7bbc4}@media screen and (max-width:1200px){.nav-link[data-v-6fa66696]{color:#ecf1ff;font-size:16px;line-height:24px}}@media screen and (max-width:1100px){.nav-link[data-v-6fa66696]{font-size:14px}}@media screen and (max-width:1000px){.nav-link[data-v-6fa66696]{font-size:16px;font-weight:400}}";
+export {
+  NavigationLink_vue_vue_type_style_index_0_scoped_6fa66696_lang as default
+};
+//# sourceMappingURL=default-styles-1.mjs-08b94fe2.js.map
