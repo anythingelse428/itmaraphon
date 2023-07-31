3e7e3a3cfa3a72e82066363858b28e9b346a8391
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-31 17:10:12 +0400

    Updates

--- .nuxt/dist/server/_nuxt/default-styles-1.mjs-17d0096e.js
+++
@@ -1,5 +0,0 @@
-const NavigationLink_vue_vue_type_style_index_0_scoped_8b23ce6b_lang = ".nav-link[data-v-8b23ce6b]{color:#fafbff;cursor:pointer;font-size:18px;font-style:normal;font-weight:600;line-height:130%;transition:all .1s}.nav-link[data-v-8b23ce6b]:hover{color:#b7bbc4}@media screen and (max-width:1200px){.nav-link[data-v-8b23ce6b]{color:#ecf1ff;font-size:16px;line-height:24px}}@media screen and (max-width:1100px){.nav-link[data-v-8b23ce6b]{font-size:14px}}@media screen and (max-width:1000px){.nav-link[data-v-8b23ce6b]{font-size:16px;font-weight:400}}";
-export {
-  NavigationLink_vue_vue_type_style_index_0_scoped_8b23ce6b_lang as default
-};
-//# sourceMappingURL=default-styles-1.mjs-17d0096e.js.map
--- .nuxt/dist/server/_nuxt/entry-styles.9d798c69.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './entry-styles-1.mjs-07cc752a.js';
-export default [style_0]
