d81f369d99a086bceea881d03b8e3d613127fb8f
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-31 06:31:35 +0400

    Updates

--- .nuxt/dist/server/_nuxt/index-styles-2.mjs-d6a5b322.js
+++
@@ -1,5 +0,0 @@
-const PaginationBullet_vue_vue_type_style_index_0_scoped_816707de_lang = '.pagination-bullet[data-v-816707de]{align-items:center;background:transparent;border-radius:100%;cursor:pointer;display:flex;height:24px;justify-content:center;margin:0 auto;position:relative;width:24px}.pagination-bullet[data-v-816707de]:before{background:#314373;border-radius:100%;content:"";display:block;height:12px;position:relative;width:12px;z-index:5}.pagination-bullet.active[data-v-816707de]{background:#1849a0}.pagination-bullet.active[data-v-816707de]:before{background:#fcfdfd}';
-export {
-  PaginationBullet_vue_vue_type_style_index_0_scoped_816707de_lang as default
-};
-//# sourceMappingURL=index-styles-2.mjs-d6a5b322.js.map
--- .nuxt/dist/server/_nuxt/index-styles.6e7cd2f3.mjs
+++
@@ -1,7 +0,0 @@
-import style_0 from './index-styles-1.mjs-5b3dbdf6.js';
-import style_1 from './index-styles-2.mjs-d6a5b322.js';
-import style_2 from './index-styles-3.mjs-f6f393fd.js';
-import style_3 from './index-styles-4.mjs-4d01fddf.js';
-import style_4 from './index-styles-5.mjs-4955184b.js';
-import style_5 from './index-styles-5.mjs-4955184b.js';
-export default [style_0, style_1, style_2, style_3, style_4, style_5]
