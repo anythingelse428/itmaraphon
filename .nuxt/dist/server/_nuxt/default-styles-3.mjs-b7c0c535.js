3021d01d5f26a5478be7fa36ce2c6265d787278c
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-31 17:20:07 +0400

    1

--- .nuxt/dist/server/_nuxt/default-styles-3.mjs-b7c0c535.js
+++
@@ -1,5 +0,0 @@
-const default_vue_vue_type_style_index_0_scoped_2cd40571_lang = ".layout[data-v-2cd40571]{margin:0;padding:0}.layout.--default[data-v-2cd40571]{background:radial-gradient(50% 50% at 50% 50%,rgba(18,27,62,0) 0,rgba(8,12,21,.5) 100%),linear-gradient(0deg,#121b3e,#121b3e),#fff;min-height:100vh}.layout.--default .container[data-v-2cd40571]{margin:0 auto;max-width:95%;position:relative;width:min(1500px,95%)}@media screen and (max-width:1670px){.layout.--default .container[data-v-2cd40571]{width:min(1200px,95%)}}@media screen and (max-width:1250px){.layout.--default .container[data-v-2cd40571]{width:min(992px,95%)}}@media screen and (max-width:1000px){.layout.--default .container[data-v-2cd40571]{max-width:100%;padding:0 90px;width:100%}}@media screen and (max-width:800px){.layout.--default .container[data-v-2cd40571]{padding:0 10px}}";
-export {
-  default_vue_vue_type_style_index_0_scoped_2cd40571_lang as default
-};
-//# sourceMappingURL=default-styles-3.mjs-b7c0c535.js.map
