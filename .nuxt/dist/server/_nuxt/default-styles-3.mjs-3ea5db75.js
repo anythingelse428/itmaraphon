0562f71cc9c85e4c9d5e0f4d79311f0cbb1c19aa
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-07-31 04:09:39 +0400

    1

---
+++ .nuxt/dist/server/_nuxt/default-styles-3.mjs-3ea5db75.js
@@ -0,0 +1,5 @@
+const default_vue_vue_type_style_index_0_scoped_274572e7_lang = ".layout[data-v-274572e7]{margin:0;padding:0}.layout.--default[data-v-274572e7]{background:radial-gradient(50% 50% at 50% 50%,rgba(18,27,62,0) 0,rgba(8,12,21,.5) 100%),linear-gradient(0deg,#121b3e,#121b3e),#fff;min-height:100vh}.layout.--default .container[data-v-274572e7]{margin:0 auto;max-width:95%;position:relative;width:min(1500px,95%)}@media screen and (max-width:1670px){.layout.--default .container[data-v-274572e7]{width:min(1200px,95%)}}@media screen and (max-width:1250px){.layout.--default .container[data-v-274572e7]{width:min(992px,95%)}}@media screen and (max-width:1000px){.layout.--default .container[data-v-274572e7]{max-width:100%;padding:0 90px;width:100%}}@media screen and (max-width:800px){.layout.--default .container[data-v-274572e7]{padding:0 10px}}";
+export {
+  default_vue_vue_type_style_index_0_scoped_274572e7_lang as default
+};
+//# sourceMappingURL=default-styles-3.mjs-3ea5db75.js.map
---
+++ .nuxt/prerender/chunks/app/_nuxt/entry-styles.97152494.mjs
@@ -0,0 +1,6 @@
+const app_vue_vue_type_style_index_0_lang = "*{box-sizing:border-box;color:#f2f5ff;font-family:Manrope,sans-serif;margin:0;padding:0;scroll-behavior:smooth}* a{text-decoration:none}body::-webkit-scrollbar{width:8px}body::-webkit-scrollbar-track{background:radial-gradient(50% 50% at 50% 50%,rgba(18,27,62,0) 0,rgba(8,12,21,.5) 100%),linear-gradient(0deg,#121b3e,#121b3e),#fff}body::-webkit-scrollbar-thumb{background:#dadada;border-radius:5px}body::-webkit-scrollbar-thumb:hover{background:#a7a7a7}";
+
+const entryStyles_97152494 = [app_vue_vue_type_style_index_0_lang];
+
+export { entryStyles_97152494 as default };
+//# sourceMappingURL=entry-styles.97152494.mjs.map
