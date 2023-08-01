469ec1ab88c7d4a09e92fc59e7a4110ad3c6d61c
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-08-02 01:26:38 +0400

    Updates

--- .nuxt/dist/server/_nuxt/index-styles-2.mjs-5d554522.js
+++
@@ -1,5 +0,0 @@
-const PaginationBullet_vue_vue_type_style_index_0_scoped_9c638fd5_lang = '.pagination-bullet[data-v-9c638fd5]{align-items:center;background:transparent;border-radius:100%;cursor:pointer;display:flex;height:24px;justify-content:center;margin:0 auto;position:relative;width:24px}.pagination-bullet[data-v-9c638fd5]:before{background:#314373;border-radius:100%;content:"";display:block;height:12px;position:relative;width:12px;z-index:5}.pagination-bullet.active[data-v-9c638fd5]{background:#1849a0}.pagination-bullet.active[data-v-9c638fd5]:before{background:#fcfdfd}';
-export {
-  PaginationBullet_vue_vue_type_style_index_0_scoped_9c638fd5_lang as default
-};
-//# sourceMappingURL=index-styles-2.mjs-5d554522.js.map
