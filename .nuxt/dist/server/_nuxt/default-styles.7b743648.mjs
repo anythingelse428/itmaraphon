f6ea12e3337b67b916336ef78130cc51ffcdee83
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-08-02 00:59:32 +0400

    0

---
+++ .nuxt/dist/server/_nuxt/default-styles.7b743648.mjs
@@ -0,0 +1,5 @@
+import style_0 from './default-styles-1.mjs-197af5b0.js';
+import style_1 from './default-styles-2.mjs-5dc2ae14.js';
+import style_2 from './default-styles-3.mjs-5ecf3c63.js';
+import style_3 from './default-styles-3.mjs-5ecf3c63.js';
+export default [style_0, style_1, style_2, style_3]
