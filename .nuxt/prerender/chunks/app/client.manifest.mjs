3cbfb0113d92a41df9c1d3261e259803da3342d1
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2023-08-03 18:53:54 +0400

    1

---
+++ .nuxt/prerender/chunks/app/client.manifest.mjs
@@ -0,0 +1,155 @@
+const client_manifest = {
+  "__plugin-vue_export-helper.c27b6911.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "_plugin-vue_export-helper.c27b6911.js"
+  },
+  "_nuxt-link.e5172fad.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "nuxt-link.e5172fad.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "layouts/default.css": {
+    "resourceType": "style",
+    "file": "default.2d6745e5.css",
+    "src": "layouts/default.css"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "default.a522a68d.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.e5172fad.js",
+      "__plugin-vue_export-helper.c27b6911.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/default.vue"
+  },
+  "default.2d6745e5.css": {
+    "file": "default.2d6745e5.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.7fc72018.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.cb3785cb.js",
+    "imports": [
+      "_nuxt-link.e5172fad.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "__plugin-vue_export-helper.c27b6911.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.7fc72018.css": {
+    "file": "error-404.7fc72018.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.c5df6088.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.749e9e3d.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "__plugin-vue_export-helper.c27b6911.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.c5df6088.css": {
+    "file": "error-500.c5df6088.css",
+    "resourceType": "style"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.6b2c2374.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "Manrope-Regular.2960175e.ttf",
+      "Manrope-SemiBold.9cb6bdf0.ttf"
+    ],
+    "css": [
+      "entry.6b2c2374.css"
+    ],
+    "dynamicImports": [
+      "layouts/default.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.54b771d8.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "entry.6b2c2374.css": {
+    "file": "entry.6b2c2374.css",
+    "resourceType": "style"
+  },
+  "Manrope-Regular.2960175e.ttf": {
+    "file": "Manrope-Regular.2960175e.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "Manrope-SemiBold.9cb6bdf0.ttf": {
+    "file": "Manrope-SemiBold.9cb6bdf0.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "pages/index.css": {
+    "resourceType": "style",
+    "file": "index.98b50a30.css",
+    "src": "pages/index.css"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.5fa398c8.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "__plugin-vue_export-helper.c27b6911.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "index.98b50a30.css": {
+    "file": "index.98b50a30.css",
+    "resourceType": "style"
+  },
+  "public/assets/fonts/Manrope/Manrope-Regular.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "Manrope-Regular.2960175e.ttf",
+    "src": "public/assets/fonts/Manrope/Manrope-Regular.ttf"
+  },
+  "public/assets/fonts/Manrope/Manrope-SemiBold.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "Manrope-SemiBold.9cb6bdf0.ttf",
+    "src": "public/assets/fonts/Manrope/Manrope-SemiBold.ttf"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
